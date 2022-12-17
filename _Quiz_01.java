397e4bf035f2c1c8e0318fb5ff154ff63f3f66d1
Garam Oh <eotjd87@naver.com>
2022-12-17 23:05:22 +0900

    chap1

---
+++ _Quiz_01.java
@@ -0,0 +1,21 @@
+package chap_01;
+
+public class _Quiz_01 {
+    public static void main(String[] args) {
+        String bus1 = "1234";
+        String bus2 = "상암08";
+
+        int minut1 = 3;
+        int minut2 = 5;
+
+        float km1 = 1.5f;
+        float km2 = 0.8f;
+
+        System.out.println(bus2 + "번 버스");
+        System.out.println("약 " + minut1 + "분 후 도착");
+        System.out.println("남은 거리 " + km1 + "km");
+
+
+
+    }
+}
